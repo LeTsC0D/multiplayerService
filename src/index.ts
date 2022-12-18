b25dc8afe201ac3022c062adc14a2ee419686ee8
shashank <shashank.srivastava5523@gmail.com>
2022-12-18 18:29:15 +0530

    service5

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+// app.use(cors());
+// const cors = require('cors');
+const corsOptions ={
+    origin:'https://facebookforfriend.netlify.app', 
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use("/", indexRouter);
 
